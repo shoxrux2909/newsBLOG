873d3fc211e8cad49b7d4db580bd02f7d286df9f
shoxrux2909 <sh.abdullayev2003@gmail.com>
2026-02-02 00:21:48 +0500

    Final web-app

---
+++ articles/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    summary = models.CharField(max_length=200, blank=True)
+    body = RichTextField()
+    photo = models.ImageField(upload_to='images/', blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[str(self.id)])
